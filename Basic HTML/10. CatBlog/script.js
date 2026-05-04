bf2d4c66482e76b761169c608e57e6fdb5d4fd37
JuanDGT <jdgt@unicauca.edu.co>
2026-05-03 21:11:47 -0500

    CatVlog

--- Basic HTML/10. CatBlog/script.js
+++ Basic HTML/10. CatBlog/script.js
@@ -1,13 +1,4 @@
-// Toggle dark mode button
-const btn = document.createElement("button");
-btn.textContent = "Toggle Dark Mode";
-btn.style.position = "fixed";
-btn.style.top = "10px";
-btn.style.right = "10px";
-btn.style.padding = "10px";
-btn.style.cursor = "pointer";
-
-document.body.appendChild(btn);
+const btn = document.getElementById("themeBtn");
 
 btn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
